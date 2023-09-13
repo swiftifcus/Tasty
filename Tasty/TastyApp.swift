054ac44c4747cfcb99973e2571f46b8b12ccca11
Marko Ilic <markilic@gmail.com>
2023-09-13 18:53:32 +0200

    Initial Commit

---
+++ Tasty/TastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyApp.swift
+//  Tasty
+//
+//  Created by Marko Ilic on 13.9.23..
+//
+
+import SwiftUI
+
+@main
+struct TastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
